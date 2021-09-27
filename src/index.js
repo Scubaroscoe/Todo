db1a0f2fc6699165ce8a26e07303091b7b8eae39
Alexander Konefal <zanderkonefal@gmail.com>
2021-09-27 17:20:41 -0400

    Added some template items. Also added a function to create the inputs that make new todo items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './style.css';
-import createTodo from './todo.js';
+import {Todo, todoForm} from './todo.js';
+import {format} from 'date-fns';
+// import 'bootstrap';
+// import {datepicker} from 'js-datepicker';
 
-console.log('This is a test message from index.js');
+let testDate = new Date(9, 27, 2021);
+console.log(`Original date: ${testDate}`);
+
+console.log(`formatted date: ${format(new Date(2021, 9, 27), 'MM/dd/yyyy')}`)
+
+let modDate = format(new Date(2021, 9, 27), 'MM/dd/yyyy');
+console.log(`Modded date is: ${modDate}`);
 
 const body = document.querySelector('body');
 const parentDiv = document.createElement('div');
@@ -9,6 +18,45 @@ parentDiv.id = 'main-div';
 
 const generalHeader = document.createElement('h1');
 generalHeader.innerHTML = "General";
-
 parentDiv.appendChild(generalHeader);
+
+// Here add logic for making something that remains on the page that will add new 
+// todo's
+// const creationDiv = document.createElement('div');
+// creationDiv.id = 'create-div';
+// const createTodoBtn = document.createElement('button');
+// createTodoBtn.textContent = "Create New Todo";
+// createTodoBtn.id = 'create-todo-btn';
+
+// const titleInput = document.createElement('input');
+// titleInput.id = "title-input";
+
+// const descInput = document.createElement('input');
+// descInput.id = "description-input";
+
+// const datePicker = document.createElement('input');
+// datePicker.type = 'date';
+// datePicker.id = 'date-input';
+
+// const priorityInput = document.createElement('input');
+// priorityInput.id = 'priority-input';
+
+// // won't add an input for checked, as checked should be false by default
+// creationDiv.append(createTodoBtn, titleInput, descInput, datePicker, priorityInput);
+// parentDiv.appendChild(creationDiv);
+let test = todoForm;
+let todoDiv = test.create();
+parentDiv.appendChild(todoDiv);
+
+const testTodo = Todo("First task", "A test task.", new Date(2021, 9, 27), 9, false);
+
+const testDiv = testTodo.getTodoDiv();
+
+const test2Todo = Todo("Second task", "Another test task.", new Date(2021, 9, 27), 8, true);
+
+const test2Div = test2Todo.getTodoDiv();
+parentDiv.appendChild(testDiv);
+parentDiv.appendChild(test2Div);
+
+
 body.appendChild(parentDiv);
--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,79 @@
-export default function createTodo(title, description, dueDate, priority, checked) {
+import {format, formatDistance, formatRElative, subDays} from 'date-fns';
+
+// format(new Date(2021, 9, 27), 'MM/dd/yyyy');
+
+function Todo(title, description, dueDate, priority, checked) {
     return {
         title: title,
         description: description, 
-        dueDate: dueDate,
+        dueDate: format(dueDate, "MM/dd/yyyy"),
         priority: priority,
-        checked: checked
+        checked: checked,
+        getTodoDiv() {
+            const todoDiv = document.createElement('div');
+            todoDiv.classList = 'todoDiv';
+            const todoContainer = document.createElement('ul');
+
+            const titleLi = document.createElement('li');
+            titleLi.textContent = title;
+            const descLi = document.createElement('li');
+            descLi.textContent = description;
+            const dueDateLi = document.createElement('li');
+            dueDateLi.textContent = this.dueDate;
+            const priorityLi = document.createElement('li');
+            priorityLi.textContent = priority;
+            const checkedLi = document.createElement('li');
+            checkedLi.textContent = checked;
+
+            todoContainer.append(titleLi, descLi, dueDateLi, priorityLi, checkedLi);
+
+            todoDiv.appendChild(todoContainer);
+            return todoDiv;
+        }
     }
 }
 
-// export {createTodo};
+// addTodo function (for creation of all the elements that make the piece of the page 
+// that has inputs for adding a new todo)
+
+const todoForm = (() => {
+    const create = () => {
+        const creationDiv = document.createElement('div');
+        creationDiv.id = 'create-div';
+        const createTodoBtn = document.createElement('button');
+        createTodoBtn.textContent = "Create New Todo";
+        createTodoBtn.id = 'create-todo-btn';
+        createTodoBtn.addEventListener('click', addTodo);
+
+        const titleInput = document.createElement('input');
+        titleInput.id = "title-input";
+
+        const descInput = document.createElement('input');
+        descInput.id = "description-input";
+
+        const datePicker = document.createElement('input');
+        datePicker.type = 'date';
+        datePicker.id = 'date-input';
+
+        const priorityInput = document.createElement('input');
+        priorityInput.id = 'priority-input';
+
+        // won't add an input for checked, as checked should be false by default
+        creationDiv.append(createTodoBtn, titleInput, descInput, datePicker, priorityInput);
+
+        return creationDiv;
+    }
+
+    function addTodo(e) {
+        let title = titleInput.innerHTML;
+        let description = descInput.innerHTML;
+        let dueDate = datePicker.innerHTML;
+        let priority = priorityInput.innerHTML;
+        let todoDiv = new Todo(title, description, dueDate, priority, false).getTodoDiv();
+        document.querySelector('body div').appendChild(todoDiv);
+    }
+
+    return {create};
+})();
+
+export {Todo, todoForm};
